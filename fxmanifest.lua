759a6d5788724a4a021427b26848382471b5f76f
Le Moon Studio <97724060+LunaLeMoon@users.noreply.github.com>
2023-06-12 03:20:15 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'LeMoon Studio'
+description 'Modification of AI airstrike system.'
+version '1.0.0'
+
+client_script 'airstrikes_client.lua'
+
